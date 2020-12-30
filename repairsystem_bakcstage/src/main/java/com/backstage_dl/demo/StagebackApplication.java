202758b906e7b1959010ae611f8d14b56ea881c6
KCSoftDL <78419406@qq.com>
2020-12-30 10:23:21 +0800

    text1

---
+++ repairsystem_bakcstage/src/main/java/com/backstage_dl/demo/StagebackApplication.java
@@ -0,0 +1,13 @@
+package com.backstage_dl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StagebackApplication.class, args);
+    }
+
+}
---
+++ repairsystem_bakcstage/src/test/java/com/backstage_dl/demo/StagebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backstage_dl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
