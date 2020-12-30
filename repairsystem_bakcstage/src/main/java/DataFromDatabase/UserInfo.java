24c883e813a9ac16381e664094e6df6aa4891f4f
KCSoftDL <78419406@qq.com>
2020-12-30 11:27:41 +0800

    提交用户数据类

---
+++ repairsystem_bakcstage/src/main/java/DataFromDatabase/UserInfo.java
@@ -0,0 +1,15 @@
+package DataFromDatabase;
+
+/**
+ * @author DL
+ * @date 2020/12/30 11:16
+ */
+
+public class UserInfo {
+    private Integer id;
+    private String username;
+    private String password;
+    private String phonenumber;
+    private String emailaddr;
+    public Integer position;
+}
---
+++ repairsystem_bakcstage/src/main/java/user_op/DataOp.java
@@ -0,0 +1,10 @@
+package user_op;
+
+/**
+ * @author DL
+ * @date 2020/12/30
+ */
+
+public class DataOp {
+
+}
