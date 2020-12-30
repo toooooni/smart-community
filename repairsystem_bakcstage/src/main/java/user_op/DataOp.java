05157f8040384379649503379b4c541fda37bbf2
KCSoftDL <78419406@qq.com>
2020-12-30 14:51:02 +0800

    update the interface document

--- repairsystem_bakcstage/src/main/java/user_op/DataOp.java
+++ repairsystem_bakcstage/src/main/java/user_op/DataOp.java
@@ -7,4 +7,17 @@ package user_op;
 
 public class DataOp {
 
+    /**
+     * 判断字符串是否属于数字
+     * @param input s
+     * @return is number?
+     */
+    private static boolean isNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
