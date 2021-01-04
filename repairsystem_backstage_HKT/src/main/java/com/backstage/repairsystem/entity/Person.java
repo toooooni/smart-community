9826378fae731c3adef8d8cb635157ef252d554b
Quentin-kt <Quentin83@163.com>
2021-01-04 17:14:30 +0800

    解决实体定义bug：大小写错误

--- repairsystem_backstage_HKT/src/main/java/com/backstage/repairsystem/entity/Person.java
+++ repairsystem_backstage_HKT/src/main/java/com/backstage/repairsystem/entity/Person.java
@@ -15,7 +15,7 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //设置主键PersonIdr为自增
     //用户Id、用户名、密码、电话、邮件、用户身份
-    private Integer PersonId;
+    private Integer personId;
     private String name;
     private String password;
     private String tel;
