7293ddb8c4267f45e6f2dca6a54d8e45cb14a93c
Quentin-kt <Quentin83@163.com>
2021-01-05 14:20:50 +0800

    更新建立数据库的代码，更新假数据

--- data.sql
+++ data.sql
@@ -1,9 +1,9 @@
 INSERT INTO `repairdb`.`person` (`person_id`, `name`, `password`, `tel`, `email`, `user_types`)
-VALUES ('1', '丁磊', '1', '110', '110@qq.com', '-1');
+VALUES ('1', '业主', '1', '110', '110@qq.com', '0');
 INSERT INTO `repairdb`.`person` (`person_id`, `name`, `password`, `tel`, `email`, `user_types`)
-VALUES ('2', '何坤廷', '2', '110', '110@qq.com', '0');
+VALUES ('2', '物业人员', '2', '110', '110@qq.com', '1');
 INSERT INTO `repairdb`.`person` (`person_id`, `name`, `password`, `tel`, `email`, `user_types`)
-VALUES ('3', '陈佳超', '3', '110', '110@qq.com', '1');
-INSERT INTO `repairdb`.`repairform` (`form_id`, `person_id`, `whetherPublic`, `itemType`, `message`, `address`, `tel`,
-                                     `whetherPay`, `stateRepair`, `repairDate`, `evaluationStar`, `evaluationNote`)
+VALUES ('3', '维修人员', '3', '110', '110@qq.com', '2');
+INSERT INTO `repairdb`.`repair_form` (`form_id`, `person_id`, `whether_public`, `item_type`, `message`, `address`, `tel`,
+                                     `whether_pay`, `state_repair`, `repair_date`, `evaluation_star`, `evaluation_note`)
 VALUES ('1', '1', '0', '家电', '无', '西安科技大学', '110', '1', '0', '2020-12-28', '1', '无');
--- repairdb.sql
+++ repairdb.sql
@@ -24,22 +24,22 @@ CREATE TABLE `person`
 -- ----------------------------
 -- Table structure for 报修单
 -- ----------------------------
-DROP TABLE IF EXISTS `repairForm`;
-CREATE TABLE `repairForm`
+DROP TABLE IF EXISTS `repair_form`;
+CREATE TABLE `repair_form`
 (
 -- 报修单号、报修人Id、是否公用、物品属性、维修备注、地址、电话、付费情况、维修状态、日期、评价星级、评价内容
     `form_id`        integer      NOT NULL auto_increment,
     `person_id`      integer      NOT NULL,
-    `whetherPublic`  bool         NOT NULL,
-    `itemType`       varchar(30)  NOT NULL,
+    `whether_public`  bool         NOT NULL,
+    `item_type`       varchar(30)  NOT NULL,
     `message`        varchar(100),
     `address`        varchar(100) NOT NULL,
     `tel`            varchar(20)  NOT NULL,
-    `whetherPay`     bool         NOT NULL,
-    `stateRepair`    integer      NOT NULL,
-    `repairDate`     date         NOT NULL,
-    `evaluationStar` integer,
-    `evaluationNote` varchar(100),
+    `whether_pay`     bool         NOT NULL,
+    `state_repair`    integer      NOT NULL,
+    `repair_date`     date         NOT NULL,
+    `evaluation_star` integer,
+    `evaluation_note` varchar(100),
     PRIMARY KEY (`form_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
